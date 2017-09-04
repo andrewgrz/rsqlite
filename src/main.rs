bd2e7cb05b162c619258354e6c420644495e22f2
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2017-09-04 10:02:47 -0500

    Add a simple REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut input = String::new();
+
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let cmd = input.trim();
+
+                match cmd {
+                    ".exit" => break,
+                    ".hello" => println!("Hello!"),
+                    _ => {
+                        println!("Unrecognized command: {}", cmd);
+                    }
+                }
+            }
+
+            Err(error) => println!("error: {}", error),
+        }
+    }
 }
